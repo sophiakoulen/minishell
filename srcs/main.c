65e49e1a02a4a8a7c9270f3a2d9d849017fd0868
Sophia Koulen <sophia.koulen@outlook.com>
2023-01-11 16:26:49 +0100

    check_args function and interactive_shell()

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,39 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 12:31:29 by znichola          #+#    #+#             */
-/*   Updated: 2023/01/11 15:44:48 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/01/11 16:25:44 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	check_args(int argc, char **argv);
+static void	interactive_shell(void);
+
 int	main(int argc, char **argv)
+{
+	check_args(argc, argv);
+	interactive_shell();
+	return (0);
+}
+
+static void	check_args(int argc, char **argv)
+{
+	const char	*msg;
+
+	(void)argv;
+	msg = "Script files are currently not supported.\nUse ./minishell to start an interactive shell.\n";
+	if (argc != 1)
+	{
+		write(2, msg, ft_strlen(msg));
+		exit(1);
+	}
+}
+
+static void	interactive_shell(void)
 {
 	char	*line;
 	
-	(void)argc;
-	(void)argv;
 	while (1)
 	{
 		line = readline("minishell$ ");
@@ -25,5 +46,4 @@ int	main(int argc, char **argv)
 			add_history(line);
 		free(line);
 	}
-	return (0);
 }
