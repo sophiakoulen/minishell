3a5eede4ac722129d53e4f9422801ad7a06e9222
znichola <znichola@student.42lausanne.ch>
2023-01-11 18:06:33 +0100

    tokniser not tested

--- includes/tokenizer.h
+++ includes/tokenizer.h
@@ -6,24 +6,47 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 14:54:08 by znichola          #+#    #+#             */
-/*   Updated: 2023/01/11 15:08:46 by znichola         ###   ########.fr       */
+/*   Updated: 2023/01/11 17:52:34 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKENIZER_H
 # define TOKENIZER_H
 
-#include "minishell.h"
+# define NUM_TOKEN_LITERALS 9
+
+const char	*tok_strings[NUM_TOKEN_LITERALS] =
+{
+	"|",
+	"<<",
+	"<",
+	">",
+	">>",
+	"(",
+	")",
+	"&&",
+	"||",
+};
 
 enum e_token_type
 {
+	e_pipe,			// |
+	e_heredoc,		// <<
+	e_in,			// <
+	e_out,			// >
+	e_out_append,	// >>
+	e_open_brace,	// (
+	e_close_brace,	// )
+	e_and,			// &&
+	e_or,			// ||
+	e_end,			// \n or \r\n
 	e_string,
-	e_symbol,
 };
 
 typedef struct s_token
 {
 	enum e_token_type	type;
+	char				*str;
 	struct s_token		*next;
 }	t_token;
 
--- srcs/tokenizer.c
+++ srcs/tokenizer.c
@@ -6,18 +6,51 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 13:42:40 by znichola          #+#    #+#             */
-/*   Updated: 2023/01/11 15:11:12 by znichola         ###   ########.fr       */
+/*   Updated: 2023/01/11 18:02:21 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	*ret_token_literal(enum e_token_type n)
+{
+	static const char	*tok_strings[NUM_TOKEN_LITERALS] = {
+		"|",
+		"<<",
+		"<",
+		">",
+		">>",
+		"(",
+		")",
+		"&&",
+		"||",
+	};
+
+	if (n > NUM_TOKEN_LITERALS || n < 0)
+		return (NULL);
+	return (tok_strings[n]);
+}
+
 t_token	*lexer(char	**str)
 {
 	t_token	*tok;
+	int		i;
 
-	while(ft_isspace())
+	tok->next = NULL;
+	i = -1;
+	while (i++ < NUM_TOKEN_LITERALS)
 	{
-
+		if (!ft_strncmp(*str, ret_token_literal(i), ft_strlen(ret_token_literal(i))))
+		{
+			tok->type = i;
+			*str = *str + ft_strlen(ret_token_literal(i));
+			return (tok);
+		}
 	}
+	i = 0;
+	while (*str[i] && ft_strncmp(*str + i, ret_token_literal(i), ft_strlen(ret_token_literal(i))))
+		i++;
+	tok->type = e_string;
+	tok->str = ft_substr(*str, 0, i);
+	return (tok);
 }
