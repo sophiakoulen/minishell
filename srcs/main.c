fe165a1146568f7a65d90c91423f5b599fc6850e
Sophia Koulen <sophia.koulen@outlook.com>
2023-01-19 11:32:11 +0100

    norminetted main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 12:31:29 by znichola          #+#    #+#             */
-/*   Updated: 2023/01/19 11:07:32 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/01/19 11:30:51 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static void	check_args(int argc, char **argv);
 static void	interactive_shell(t_env *env);
+static int	exec_line(char *line);
 
 /*
 	alternative method to find the env variable
@@ -21,67 +22,74 @@ static void	interactive_shell(t_env *env);
 	extern char	**environ;
  */
 
-// int	main(int argc, char **argv, char **envp)
 int	main(int argc, char **argv, char **envp)
 {
-	t_env *env;
+	t_env	*env;
 
 	env = init_env(envp);
-
 	check_args(argc, argv);
-
 	interactive_shell(env);
 	return (0);
 }
 
 static void	check_args(int argc, char **argv)
 {
-	const char	*msg;
+	const char	*msg1;
+	const char	*msg2;
 
 	(void)argv;
-	msg = "Script files are currently not supported.\nUse ./minishell to start an interactive shell.\n";
+	msg1 = "Script files are currently not supported.\n";
+	msg2 = "Use ./minishell to start an interactive shell.\n";
 	if (argc != 1)
 	{
-		write(2, msg, ft_strlen(msg));
-		exit(1);
+		write(2, msg1, ft_strlen(msg1));
+		write(2, msg2, ft_strlen(msg2));
+		exit(2);
 	}
 }
 
 static void	interactive_shell(t_env *env)
 {
-	char				*line;
-	t_token				*tok_list;
-	t_token				*start;
-	t_parsed_pipeline	*intermediate;
-	t_pipeline			*p;
+	char	*line;
 
 	(void)env;
 	while (1)
 	{
 		line = readline("minishell$ ");
-		if (line && *line)
+		if (!line)
+			break ;
+		if (*line)
 			add_history(line);
-
-		tok_list = construct_tok_list(line);
-		start = tok_list;
-
-		if (start->type != e_end)
-		{
-			intermediate = x_malloc(sizeof(*intermediate), 1);
-
-			if (parse_pipeline(intermediate, &tok_list) == SUCCESS)
-			{
-				p = expand_pipeline(intermediate);
-
-				exec_pipeline(p);
-
-				pipeline_cleanup(p);
-				free(p);
-			}
-		}
-
-		tok_list_cleanup(start);
-
+		exec_line(line);
 		free(line);
 	}
+}
+
+static int	exec_line(char *line)
+{
+	int					ret;
+	t_token				*tok_list;
+	t_token				*start;
+	t_parsed_pipeline	*parsed;
+	t_pipeline			*pipeline;
+
+	ret = 0;
+	tok_list = construct_tok_list(line);
+	start = tok_list;
+	if (start->type != e_end)
+	{
+		parsed = x_malloc(sizeof(*parsed), 1);
+		if (parse_pipeline(parsed, &tok_list) == SUCCESS)
+		{
+			pipeline = expand_pipeline(parsed);
+			ret = exec_pipeline(pipeline);
+			pipeline_cleanup(pipeline);
+			free(pipeline);
+		}
+		else
+			ret = 2;
+		parsed_pipeline_cleanup(parsed);
+	}
+	tok_list_cleanup(start);
+	return (ret);
 }
