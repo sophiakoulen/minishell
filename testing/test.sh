328ae2f14624ce3ea201e777cd23cfe7022a2857
Sophia Koulen <sophia.koulen@outlook.com>
2023-01-20 12:31:04 +0100

    started writing a test scripting but it's not really working yet

--- testing/test.sh
+++ testing/test.sh
@@ -1,7 +1,38 @@
 #/bin/bash
 
-MINISHELL= ../minishell
+MINI=../minishell
 
-$MINISHELL >out <<END
-hello
+echo testing minishell...
+
+# test 0
+$MINI >>act_out0 2>>act_err0 <<END
+|
 END
+bash >>exp_out0 2>>exp_err0 <<END
+|
+END
+diff act_out0 exp_out0 >>test0.diff
+diff act_err0 exp_out0 >>test0.diff
+rm *0
+
+# test 1
+$SH >>act_out1 2>>act_err1 <<END
+echo |
+END
+bash >>exp_out1 2>>exp_err1 <<END
+echo |
+END
+diff act_out1 exp_out1 >>test1.diff
+diff act_err1 exp_out1 >>test1.diff
+rm *1
+
+# test 2
+$SH >>act_out2 2>>act_err2 <<END
+|||<>
+END
+bash >>exp_out2 2>>exp_err2 <<END
+|||<>
+END
+#diff act_out2 exp_out2 >>test2.diff
+#diff act_err2 exp_out2 >>test2.diff
+#rm *2
