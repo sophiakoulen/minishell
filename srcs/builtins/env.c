66e6375c546cf1845dc85e275b65f0c27dd90b92
Sophia Koulen <sophia.koulen@outlook.com>
2023-01-20 16:25:47 +0100

    some comments

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,29 +6,19 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 21:47:55 by znichola          #+#    #+#             */
-/*   Updated: 2023/01/19 12:43:36 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/01/20 15:50:44 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /*
-	env builtin, prints directly to screen
-	and cleans up after it's self
- */
+	env builtin: print environment variables to stdout
+
+	There is no defined order that the variables have to be printed in.
+*/
 int	exec_env(char **args)
 {
 	strarr_print(args);
 	return (0);
 }
-
-
-// void	print_env(t_env *env)
-// {
-// 	if (env == NULL)
-// 		return ;
-// 	// ft_printf("%29s = %s\n", env->key, env->value);
-// 	ft_printf("%s=%s\n", env->key, env->value);
-
-// 	print_env(env->next);
-// }
--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -3,35 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 18:29:52 by znichola          #+#    #+#             */
-/*   Updated: 2023/01/18 19:03:23 by znichola         ###   ########.fr       */
+/*   Updated: 2023/01/20 16:18:37 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+	exit builtin: exit the shell
+
+	If an argument is supplied:
+		If the argument is numeric, it is used as the exit status.
+		If it isn't numeric, an error is printed and the exit status is 255.
+	If no argument is supplied, the exit code is that of the last command.
+*/
 int	exec_exit(char **args)
 {
 	int	ret_code;
 	int	i;
 
 	i = 0;
-	ret_code = 0;
+	ret_code = 0; //fetch exit code from last command here!!
 	while (args && *args)
 	{
 		if (i > 0)
 		{
-			ft_printf("exit: too many arguments\n");
+			ft_printf("minishell: exit: too many arguments\n"); //error should be printed to stderr
 			return (1);
+		}
+		if (!is_numeric(*args))
+		{
+			ft_printf("minishell: exit: %s: numeric argument required\n", *args); //on STDERR!!
+			exit(255);
 		}
 		ret_code = ft_atoi(*args);
 		args++;
 		i++;
 	}
-	// cleanup functions? idk really.
-	// TODO: return the correct exit code, from the last run command
+	// cleanup functions?
 	exit(ret_code);
 	return (0);
+}
+
+int	is_numeric(char *str)
+{
+	if (*str == '-' || *str == '+')
+		str++;
+	while (ft_isdigit(*str))
+		str++;
+	return (*str == '\0');
 }
