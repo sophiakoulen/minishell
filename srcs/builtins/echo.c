51c3088d108209e5fe9127e2eeb7eaf3504f7647
Sophia Koulen <sophia.koulen@outlook.com>
2023-01-20 15:44:16 +0100

    fixed #61, norminetted echo.c, some comments

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -3,44 +3,85 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 12:52:30 by znichola          #+#    #+#             */
-/*   Updated: 2023/01/18 19:38:14 by znichola         ###   ########.fr       */
+/*   Updated: 2023/01/20 15:43:30 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+#define NO_NEWLINE 1
+
+static int	parse_options(char ***args);
+static int	is_valid_option(char *arg);
+
+/*
+	echo builtin: Print arguments to stdout.
+	
+	Arguments are separated by spaces and followed by a newline.
+	If option "-n" is specified, the newline is ommited.
+*/
 int	exec_echo(char **args)
 {
 	char	*line;
-	int		no_newline;
-	int		i;
+	int		options;
 
-	if (!args || !*args)
-		return (0);
-	no_newline = 0;
-	if (args[0][0] == '-' && args[0][1] == 'n')
-	{
-		i = 2;
-		while (args[0][i] == 'n')
-			i++;
-		if (args[0][i] == '\0')
-		{
-			no_newline = 1;
-			args++;
-		}
-	}
+	if (!args)
+		return (2);
+	options = parse_options(&args);
 	while (*args)
 	{
 		line = *args;
 		ft_printf("%s", line);
-		if (!no_newline && args[1] == NULL)
-			ft_printf("\n");
-		else if (args[1])
+		if (*(args + 1))
 			ft_printf(" ");
 		args++;
+	}
+	if (!(options & NO_NEWLINE))
+		ft_printf("\n");
+	return (0);
+}
+
+/*
+	Advance the array pointer while valid options are found.
+	Return value is an integer representing the found options,
+	bitwise-OR-ed together.
+
+	Currently, the only supported option is "-n", and means
+	"no newline".
+*/
+static int	parse_options(char ***args)
+{
+	int	options;
+
+	options = 0;
+	while (*args && is_valid_option(**args))
+	{
+		options |= NO_NEWLINE;
+		(*args)++;
+	}
+	return (options);
+}
+
+/*
+	Checks if a string is a valid option.
+
+	Currently, only "-n" is a valid option.
+*/
+static int	is_valid_option(char *arg)
+{
+	int	i;
+
+	if (!arg)
+		return (0);
+	if (strncmp(arg, "-n", 2) == 0)
+	{
+		i = 2;
+		while (arg[i] == '\n')
+			i++;
+		return (arg[i] == '\0');
 	}
 	return (0);
 }
