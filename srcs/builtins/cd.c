be5157edbb4da10eb395ee34f094f873e7317fa4
znichola <znichola@student.42lausanne.ch>
2023-01-25 17:24:11 +0100

    fix #67 cd updates env

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 13:29:21 by skoulen           #+#    #+#             */
-/*   Updated: 2023/01/23 16:27:23 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/01/25 17:23:08 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,28 @@
 */
 int	exec_cd(char **args, t_env **env, int prev)
 {
-	(void)env;
+	char	newpwd[PATH_MAX + 1];
+	char	*oldpwd;
+
 	(void)prev;
 	if (!*args)
 		return (0);
 	if (chdir(*args) != 0)
 	{
 		print_error("cd", *args, strerror(errno));
+		return (1);
+	}
+	if (getcwd(newpwd, sizeof(newpwd)) != NULL)
+	{
+		oldpwd = ret_env_key(*env, "PWD");
+		if (oldpwd == NULL)
+			oldpwd = ft_strdup("");
+		env_add(env, "PWD", newpwd);
+		env_add(env, "OLDPWD", oldpwd);
+	}
+	else
+	{
+		print_error("pwd", 0, strerror(errno));
 		return (1);
 	}
 	return (0);
