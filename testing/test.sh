510df6cd4c77ec8e771be02e89c3b37a1ca4d89d
Sophia Koulen <sophia.koulen@outlook.com>
2023-01-26 12:56:11 +0100

    updated test folder

--- testing/test.sh
+++ testing/test.sh
@@ -4,35 +4,14 @@ MINI=../minishell
 
 echo testing minishell...
 
-# test 0
-$MINI >>act_out0 2>>act_err0 <<END
-|
-END
-bash >>exp_out0 2>>exp_err0 <<END
-|
-END
-diff act_out0 exp_out0 >>test0.diff
-diff act_err0 exp_out0 >>test0.diff
-rm *0
+#printf "echo hello" | $MINI >actual
 
-# test 1
-$SH >>act_out1 2>>act_err1 <<END
-echo |
-END
-bash >>exp_out1 2>>exp_err1 <<END
-echo |
-END
-diff act_out1 exp_out1 >>test1.diff
-diff act_err1 exp_out1 >>test1.diff
-rm *1
+#printf "echo hello" | bash >expected
 
-# test 2
-$SH >>act_out2 2>>act_err2 <<END
-|||<>
-END
-bash >>exp_out2 2>>exp_err2 <<END
-|||<>
-END
-#diff act_out2 exp_out2 >>test2.diff
-#diff act_err2 exp_out2 >>test2.diff
-#rm *2
+$MINI >actual <<EOF
+echo hello
+EOF
+
+bash >expected <<EOF
+echo hello
+EOF
