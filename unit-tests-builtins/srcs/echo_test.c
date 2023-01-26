347a5b88fcbe51bc98f99c1da7ae51b1dc6524d8
znichola <znichola@student.42lausanne.ch>
2023-01-26 12:56:03 +0100

    builtins fixed

--- unit-tests-builtins/srcs/echo_test.c
+++ unit-tests-builtins/srcs/echo_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 13:04:35 by znichola          #+#    #+#             */
-/*   Updated: 2023/01/20 17:48:01 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/01/26 12:35:39 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ int	main(int argc, char **argv)
 {
 	if(argc == 1)
 		return(0);
-	return (exec_builtin(e_echo, argv + 1, 0));
+	return (exec_builtin(e_echo, argv + 1, 0, 42));
 }
