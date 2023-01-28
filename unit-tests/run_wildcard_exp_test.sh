bcfa28ceb49a3b79045d3cba6fd68e41357e53a6
znichola <niki.zivkovic@hotmail.com>
2023-01-28 23:49:05 +0100

    wildcard test script

--- unit-tests/run_wildcard_exp_test.sh
+++ unit-tests/run_wildcard_exp_test.sh
@@ -11,6 +11,9 @@ echo "\n${ITALIC}${YELLOW}Testing the $N${RESET}"
 # these tests where written by me, they are not infallable
 # exec_test <test exicutable> <test number> <expected result> <arg1> <arg2> <..>
 
-exec_test $N 0 "*.sh" '*.sh'
-exec_test $N 1 "run*.sh" 'run*.sh'
-exec_test $N 1 "run*test*sh" 'run*test*sh'
+exec_test $N 0 "*" '*'
+exec_test $N 1 "*.sh" '*.sh'
+exec_test $N 2 "run*" 'run*'
+exec_test $N 3 "*_*" '*_*'
+exec_test $N 4 "run*.sh" 'run*.sh'
+exec_test $N 5 "run*test*sh" 'run*test*sh'
