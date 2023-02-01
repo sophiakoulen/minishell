fb177197413cf7dd78b0c5b25a678c52258b9af9
Sophia Koulen <sophia.koulen@outlook.com>
2023-02-01 12:23:47 +0100

    fix #89 - cd without args now goes to /Users/skoulen

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -3,44 +3,66 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 13:29:21 by skoulen           #+#    #+#             */
-/*   Updated: 2023/01/25 17:23:08 by znichola         ###   ########.fr       */
+/*   Updated: 2023/02/01 12:23:07 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	update_env(t_env **env);
+
 /*
 	cd builtin: Change working directory.
 
-	TO DO: Update $PWD and $OLDPWD variables.
+	If no argument is given, we look at the HOME env variable.
+	After successful changing of working directory, we update
+	the PWD and OLDPWD env variables.
 */
 int	exec_cd(char **args, t_env **env, int prev)
+{
+	char	*home;
+	char	*path;
+
+	(void)prev;
+	if (!*args)
+	{
+		home = ret_env_key(*env, "HOME");
+		if (!home || !*home)
+		{
+			print_error("cd", 0, "HOME not set");
+			return (1);
+		}
+		path = home;
+	}
+	else
+		path = *args;
+	if (chdir(path) != 0)
+	{
+		print_error("cd", path, strerror(errno));
+		return (1);
+	}
+	if (update_env(env) != 0)
+		return (1);
+	return (0);
+}
+
+static int	update_env(t_env **env)
 {
 	char	newpwd[PATH_MAX + 1];
 	char	*oldpwd;
 
-	(void)prev;
-	if (!*args)
-		return (0);
-	if (chdir(*args) != 0)
-	{
-		print_error("cd", *args, strerror(errno));
-		return (1);
-	}
 	if (getcwd(newpwd, sizeof(newpwd)) != NULL)
 	{
 		oldpwd = ret_env_key(*env, "PWD");
-		if (oldpwd == NULL)
-			oldpwd = ft_strdup("");
 		env_add(env, "PWD", newpwd);
 		env_add(env, "OLDPWD", oldpwd);
 	}
 	else
 	{
-		print_error("pwd", 0, strerror(errno));
+		print_error("cd", 0, strerror(errno));
 		return (1);
 	}
 	return (0);
