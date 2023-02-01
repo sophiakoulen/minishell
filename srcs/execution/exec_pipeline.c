48d0fe3cdaa236ebdef8bb51cc1f24fd6bd1b817
Sophia Koulen <sophia.koulen@outlook.com>
2023-02-01 12:49:33 +0100

    tried simplifiying simple builtins but broken ret

--- srcs/execution/exec_pipeline.c
+++ srcs/execution/exec_pipeline.c
@@ -6,13 +6,11 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 14:03:02 by skoulen           #+#    #+#             */
-/*   Updated: 2023/02/01 11:57:22 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/02/01 12:46:44 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static int	simple_command(t_exec *exec);
 
 /*
 	Execute pipeline and return exit status of last command.
@@ -26,29 +24,14 @@ int	exec_pipeline(t_list *pipeline, t_env **env, int prev)
 
 	exec = prepare_pipeline(pipeline, env, prev);
 	ret = 0;
-	if (exec->n == 1)
-		ret = simple_command(exec);
-	else if (exec->n > 1)
+	if (exec->n == 1 && exec->cmds[0].builtin != -1)
+	{
+		ret = launch_builtin(exec, 0);
+		printf("%d %d\n", ret, compute_return_value(ret));
+	}
+	else if (exec->n >= 1)
 		ret = multiple_commands(exec);
 	cleanup_exec(exec);
 	free(exec);
-	return (ret);
-}
-
-static int	simple_command(t_exec *exec)
-{
-	int	*pids;
-	int	ret;
-
-	pids = NULL;
-	if (exec->cmds[0].builtin != -1)
-		ret = launch_builtin(exec, 0);
-	else
-		pids = launch_all(exec);
-	write_all_heredocs(exec);
-	close_fds(exec);
-	if (exec->cmds[0].builtin == -1)
-		waitpid(*pids, &ret, 0);
-	free(pids);
 	return (compute_return_value(ret));
 }
