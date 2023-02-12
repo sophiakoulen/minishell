dc26c0deb86f9b7602162e2a0c2fd73c503a5882
Sophia Koulen <sophia.koulen@outlook.com>
2023-02-12 17:33:53 +0100

    minor changes to init_env renaming incrament_shlvl to increment_shlvl renaming assigne_and_respond to assign_and_respond making more clear the errors are about the shlvl

--- srcs/environment/env_init.c
+++ srcs/environment/env_init.c
@@ -6,14 +6,14 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 12:41:48 by skoulen           #+#    #+#             */
-/*   Updated: 2023/02/12 16:58:48 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/02/12 17:16:48 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	incrament_shlvl(t_env **env);
-static void	assigne_and_respond(int *lvl, char **value);
+static void	increment_shlvl(t_env **env);
+static void	assign_and_respond(int *lvl, char **value);
 
 /*
 	returns allocated linked list of env structs.
@@ -38,47 +38,43 @@ t_env	*init_env(char **envp)
 		free(value);
 		envp++;
 	}
-	incrament_shlvl(&ret);
+	increment_shlvl(&ret);
 	return (ret);
 }
 
 /*
 	incraments the shell level
 */
-static void	incrament_shlvl(t_env **env)
+static void	increment_shlvl(t_env **env)
 {
 	char	*value;
 	int		lvl;
 
 	value = ret_env_key(*env, "SHLVL");
-	if (value == NULL)
-		lvl = 0;
-	else
-	{
-		assigne_and_respond(&lvl, &value);
-	}
+	assign_and_respond(&lvl, &value);
 	value = ft_itoa(lvl);
 	env_add(env, "SHLVL", value);
 	free(value);
 }
 
-static void	assigne_and_respond(int *lvl, char **value)
+static void	assign_and_respond(int *lvl, char **value)
 {
 	if (safe_atoi(lvl, value))
 	{
-		print_error(0, 0, "shell level is too damn high, resting to 0");
+		print_error(0, "SHLVL warning",
+			"`shell level is too damn high, reseting to 0'");
 		*lvl = 0;
 	}
 	if (*lvl < 0)
 	{
-		print_error(0, 0,
-			"don't like this negative shell enegry, resetting to 0");
+		print_error(0, "SHLVL warning",
+			"`don't like this negative shell energy, reseting to 0'");
 		*lvl = 0;
 	}
-	if (safe_add(lvl, 1))
+	else if (safe_add(lvl, 1))
 	{
-		print_error(0, 0,
-			"tried to catch me in an overflow! well I reset you to 0");
+		print_error(0, "SHLVL warning",
+			"`tried to catch me in an overflow! well I reset you to 0'");
 		*lvl = 0;
 	}
 }
