sophiakoulen/minishell

commit 7ee8c5aac759b1900406da274ba6d5657bea513f

changed where extract-path is defined and modified function slightly

srcs/execution/prep_pipeline2.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   prep_pipeline2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:57:32 by skoulen           #+#    #+#             */
-/*   Updated: 2023/02/10 12:55:48 by znichola         ###   ########.fr       */
+/*   Updated: 2023/02/12 18:21:13 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static int	**get_pipes(int n);
+static char	**extract_path(t_env *env);
 
 void	init_exec(t_list *pipeline, t_exec *exec, t_env **env, int prev)
 {
-	char	**strs;
 	int		n_pipes;
 
 	exec->n = ft_lstsize(pipeline);
@@ -29,9 +29,7 @@ void	init_exec(t_list *pipeline, t_exec *exec, t_env **env, int prev)
 	exec->fd_count = n_pipes * 2 + exec->n * 2;
 	exec->env = env;
 	exec->prev = prev;
-	strs = env_to_strarr(*env);
-	exec->path = extract_path(strs);
-	strarr_cleanup(strs);
+	exec->path = extract_path(*env);
 	exec->cmds = x_malloc(sizeof(*(exec->cmds)), exec->n);
 }
 
@@ -53,4 +51,37 @@ static int	**get_pipes(int n)
 		i++;
 	}
 	return (pipes);
+}
+
+/*
+	extract_path:
+
+	Extracts the path variable from the environment,
+	in the form of a null-terminated array of strings.
+
+	If the PATH variable isn't set (not the same as empty),
+	a default value for the path is used.
+*/
+static char	**extract_path(t_env *env)
+{
+	int		i;
+	char	**default_path;
+	char	**strs;
+
+	strs = env_to_strarr(env);
+	if (strs)
+	{
+		i = 0;
+		while (strs[i])
+		{
+			if (ft_strncmp("PATH=", strs[i], 5) == 0)
+			{
+				return (ft_split(strs[i] + 5, ':'));
+			}
+			i++;
+		}
+	}
+	strarr_cleanup(strs);
+	default_path = ft_split("", ':');
+	return (default_path);
 }

includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 12:31:34 by znichola          #+#    #+#             */
-/*   Updated: 2023/02/11 16:25:22 by znichola         ###   ########.fr       */
+/*   Updated: 2023/02/12 18:21:31 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,7 +112,6 @@ int			find_cmd(char **path, char *filename, char **res);
 
 /* find_cmd_utils */
 
-char		**extract_path(char **envp);
 int			has_slashes(const char *str);
 char		*concat_slash(const char *str1, const char *str2);
 int			file_ok(char *filename, char **res);

commit 6717b7e203c07971a29fcf0befecf49904384db7

undo leak i caused in commit 7ee8c5aac759b1900406da274ba6d5657bea513f extract_path() was leaking like a sieve

srcs/execution/prep_pipeline2.c
@@ -6,7 +6,7 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:57:32 by skoulen           #+#    #+#             */
-/*   Updated: 2023/02/13 14:34:12 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/02/15 14:36:45 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,23 +78,26 @@ static int	**get_pipes(int n)
 static char	**extract_path(t_env *env)
 {
 	int		i;
-	char	**default_path;
-	char	**strs;
+	char	**env_array;
+	char	**ret;
 
-	strs = env_to_strarr(env);
-	if (strs)
+	ret = NULL;
+	env_array = env_to_strarr(env);
+	if (env_array)
 	{
 		i = 0;
-		while (strs[i])
+		while (env_array[i])
 		{
-			if (ft_strncmp("PATH=", strs[i], 5) == 0)
+			if (ft_strncmp("PATH=", env_array[i], 5) == 0)
 			{
-				return (ft_split(strs[i] + 5, ':'));
+				ret = ft_split(env_array[i] + 5, ':');
+				break ;
 			}
 			i++;
 		}
 	}
-	strarr_cleanup(strs);
-	default_path = ft_split("", ':');
-	return (default_path);
+	strarr_cleanup(env_array);
+	if (ret)
+		return (ret);
+	return (ft_split("", ':'));
 }