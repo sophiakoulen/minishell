69911784eeff6fbaad258f255d0c89a1352c86f0
Sophia Koulen <sophia.koulen@outlook.com>
2023-02-15 17:26:15 +0100

    maybe use a contains_wildcards function?

--- srcs/expansion/wildcard_exp.c
+++ srcs/expansion/wildcard_exp.c
@@ -6,7 +6,7 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 13:02:38 by znichola          #+#    #+#             */
-/*   Updated: 2023/02/15 16:59:02 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/02/15 17:18:07 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int		match_wildcard(char *file, char *expr);
 int		check_starting_wildcard(int *i, char **file, char *expr, char ***chunks);
 int		check_ending_wildcard(int i, char *file, char *expr, char **chunks);
 int		check_single_chunk(int i, int *ret, char **file, char **chunks);
+int		contains_wildcard(char *str);
 
 /*
 	TODO:
@@ -79,6 +80,7 @@ int	match_wildcard(char *file, char *expr)
 	ret = -1;
 	expr = ft_strdup(expr);
 	chunks = wild_split(expr, '*');
+	dprintf(2, "chunks[0]: {%s}\n", chunks[0]);
 	quote_removal_strarr(chunks);
 	i = 0;
 	if (check_starting_wildcard(&i, &file, expr, &chunks) == 1)
@@ -186,4 +188,18 @@ int	check_ending_wildcard(int i, char *file, char *expr, char **chunks)
 		return (ft_strcmp(file, chunks[i - 1]) == 0);
 	}
 	return (1);
+}
+
+int contains_wildcard(char *str)
+{
+	int	i;
+	int	state;
+
+	state = 0;
+	while (str[i] && (str[i] != '*' || state))
+	{
+		update_state(str, &state);
+		i++;
+	}
+	return (str[i] == '*');
 }
