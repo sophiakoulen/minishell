295453ede47d6d500b5e7ce8270db4369e304479
Sophia Koulen <sophia.koulen@outlook.com>
2023-02-16 13:30:48 +0100

    fix #150

--- srcs/expansion/wildcard_exp.c
+++ srcs/expansion/wildcard_exp.c
@@ -6,7 +6,7 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 13:02:38 by znichola          #+#    #+#             */
-/*   Updated: 2023/02/15 17:18:07 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/02/16 13:27:41 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int		match_wildcard(char *file, char *expr);
 int		check_starting_wildcard(int *i, char **file, char *expr, char ***chunks);
 int		check_ending_wildcard(int i, char *file, char *expr, char **chunks);
 int		check_single_chunk(int i, int *ret, char **file, char **chunks);
-int		contains_wildcard(char *str);
 
 /*
 	TODO:
@@ -80,7 +79,6 @@ int	match_wildcard(char *file, char *expr)
 	ret = -1;
 	expr = ft_strdup(expr);
 	chunks = wild_split(expr, '*');
-	dprintf(2, "chunks[0]: {%s}\n", chunks[0]);
 	quote_removal_strarr(chunks);
 	i = 0;
 	if (check_starting_wildcard(&i, &file, expr, &chunks) == 1)
@@ -185,21 +183,8 @@ int	check_ending_wildcard(int i, char *file, char *expr, char **chunks)
 	{
 		file -= ft_strlen(chunks[i - 1]);
 		file += ft_strlen(file) - ft_strlen(chunks[i - 1]);
-		return (ft_strcmp(file, chunks[i - 1]) == 0);
+		if (!*chunks[i - 1] || ft_strcmp(file, chunks[i - 1]) != 0)
+			return (0);
 	}
 	return (1);
-}
-
-int contains_wildcard(char *str)
-{
-	int	i;
-	int	state;
-
-	state = 0;
-	while (str[i] && (str[i] != '*' || state))
-	{
-		update_state(str, &state);
-		i++;
-	}
-	return (str[i] == '*');
 }
